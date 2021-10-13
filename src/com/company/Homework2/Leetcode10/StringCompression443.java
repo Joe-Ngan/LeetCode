cb424968c764595df6bbe937e61a4022cb89c7a3
Joe-Ngan <yan.jinz@northeastern.edu>
2021-10-12 20:08:29 -0700

    Homework 2: Change the note language

--- src/com/company/Homework2/Leetcode10/StringCompression443.java
+++ src/com/company/Homework2/Leetcode10/StringCompression443.java
@@ -9,34 +9,34 @@ public class StringCompression443 {
         int i = 0;
         int write = 0;
         int n = chars.length;
-        //特殊性检验
+        //special situation check
         if(n==1) return chars.length;
 
         while(i<n){
             int idx = i;
-            //从i开始，找到同字符的终点idx
+            //from i to idx where are all same characters
             while(idx < n && chars[idx]==chars[i]) idx++;
-            //长度为idx-i（区间为：[i,idx]）
+            //length：[i,idx]）
             int len = idx -i;
-            //1.写字符
+            //1.write : char
             chars[write++]=chars[i];
-            //2.写数字（若长度>1）
+            //2.write : num（len>1）
             if(len>1){
                 int startNum = write;
                 int endNum = startNum;
-                //数字是多位数
+                //more than 1 digit
                 while(len != 0){
-                    //取最小位数
+                    //mode
                     chars[endNum++]= (char) ((len % 10)+'0');
-                    //除10取次小位数
+                    //divide
                     len /= 10;
                 }
-                //将长度翻转
+                //reverse the num
                 reverse(chars, startNum, endNum-1);
-                //下次书写位置即为end
+                //relocate write
                 write=endNum;
             }
-            //下一个起始点：从i开始，找到同字符的终点
+            //next start idx
             i = idx;
         }
         return write;
